69df686c4d491e2b012c586684f7717ff241f9d1
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-03-08 01:04:46 +0100

    Files manager
    promisify comp?

---
+++ utils/redis.js
@@ -0,0 +1,43 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.log(err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(reply);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
